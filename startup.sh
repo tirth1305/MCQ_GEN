6f919e8d04860c667bca3eb43c7acc9c8499bd2c
Tirth Patel <142647208+tirth1305@users.noreply.github.com>
2025-07-08 14:54:04 +0530

    Delete startup.sh

--- startup.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-chmod +x startup.sh
-echo "Installing dependencies..."
-pip install --no-cache-dir -r requirements.txt
-
-echo "Starting Streamlit app..."
-streamlit run app.py --server.port=8000 --server.address=0.0.0.0
